723f56ce687c54f09de422fadcaa46f7e9b3baed
Dmytro Baranetskyi <ckopnio@gmail.com>
2012-11-26 14:58:03 +0200

    Added english lanquage

---
+++ admin/language/english/module/worktime.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title'] = 'Time';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module Time!';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_mon']            = 'Monday';
+$_['text_tue']            = 'Tuesday';
+$_['text_wed']            = 'Wednesday';
+$_['text_thu']            = 'Thursday';
+$_['text_fri']            = 'Friday';
+$_['text_sat']            = 'Saturday';
+$_['text_sun']            = 'Sunday';
+$_['text_description_top'] = 'Signature top';
+$_['text_description_bot'] = 'Signature bottom';
+
+// Entry
+$_['entry_layout'] = 'Scheme';
+$_['entry_position'] = 'Location:';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort';
+
+// Error
+$_['error_permission'] = 'You are not authorized to change the unit of time';
+$_['error_time'] = 'Enter the work';
+?>
---
+++ catalog/language/english/module/worktime.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title'] = 'Work:';
+
+// Text
+$_['text_mon'] = 'Monday';
+$_['text_tue'] = 'Tuesday';
+$_['text_wed'] = 'Wednesday';
+$_['text_thu'] = 'Thursday';
+$_['text_fri'] = 'Friday';
+$_['text_sat'] = 'Saturday';
+$_['text_sun'] = 'Sunday';
+$_['text_dayoff'] = 'off';
+
+?>
